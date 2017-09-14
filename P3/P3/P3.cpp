5e2681f6b655e3d1a53397acf098735b61f66e9d
IslamAlahovich <kusochec@gmail.com>
2017-09-14 14:10:19 +0700

    add P3, CompS dir

---
+++ P3/P3/P3.cpp
@@ -0,0 +1,16 @@
+// P3.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "hello pidor" << endl;
+
+    return 0;
+}
+
