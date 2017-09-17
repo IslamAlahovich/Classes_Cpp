b49eaefef28cf9828d0cea69ab34b43d0370de54
IslamAlahovich <kusochec@gmail.com>
2017-09-17 20:30:54 +0700

    complete P3

---
+++ P4/P4/P4.cpp
@@ -0,0 +1,13 @@
+// P4.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include "matplotlibcpp.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello pidor" << endl;
+
+	return 0;
+}
